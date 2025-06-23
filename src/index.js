ad6ebd77fc4293e310db61c78252df2837209be5
Premanshc <premanshacademics@gmail.com>
2025-06-23 14:08:05 +0530

    fixedb buges

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ app.use(cors({
   origin: 'https://military-asset-management-system-fr.vercel.app',
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
-  credentials: true
+  credentials: true,
 }));
 
 app.options('*', cors());
